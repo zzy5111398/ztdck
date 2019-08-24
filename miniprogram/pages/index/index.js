2fd120d7f70722ef6673c7b8fd16dc7a3f0d7bc3
zzy5111398 <zzy5111398@live.com>
2019-08-24 20:07:49 +0800

    单词记录上传数据功能

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,11 +1,13 @@
 // miniprogram/pages/index/index.js
+const app = getApp()
+const db = wx.cloud.database()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    wlist:[],
   },
 
   /**
@@ -26,7 +28,17 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    var that = this;
+    db.collection('words').get({
+      success: function (res) {
+        // res.data 是一个包含集合中有权限访问的所有记录的数据，不超过 20 条
+        console.log(res.data);
+        that.setData({
+          //第一个data为固定用法
+          wlist: res.data
+        })
+      }
+    })
   },
 
   /**
@@ -62,5 +74,35 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  saveword: function (e) {
+    var form = e.detail.value;
+    var that = this;
+    db.collection('words').add({
+      // data 字段表示需新增的 JSON 数据
+      data: {
+        // _id: 'todo-identifiant-aleatoire', // 可选自定义 _id，在此处场景下用数据库自动分配的就可以了
+        word:form.word,
+        description:form.description
+      },
+      success: function (res) {
+        // res 是一个对象，其中有 _id 字段标记刚创建的记录的 id
+        console.log(res),
+        that.onShow()
+      }
+    })
+  },
+
+  delword: function (e) {
+    var that = this;
+    if (e.currentTarget.dataset.id != 0) {
+      db.collection('words').doc(e.currentTarget.dataset.id).remove({
+        success: function (res) {
+          console.log(res.data),
+          that.onShow()
+        }
+      })
+    }
   }
 })
